4800b878137854d249cfca73f1dc185ffa96d402
Joao <progjvdallagnol2001@gmail.com>
2022-06-02 22:03:48 -0300

    configurando o index.js, criando a rota inicial e fazendo a conexão com o DB, criando o schema 'blog'

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
+const connection = require("./database/connection")
+
 
 app.listen("8484", () => {
     console.log("O servidor está rodando!")
+})
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com sucesso!")
+    }).catch((error) => {
+        console.log(error)
+    })
+
+app.get("/", (req, res) => {
+    res.send("Rota incial!")
 })
